95a7ee8a02bded078e38412d67884aaa02921c75
seeoonghoo <seeoonghoo@gmail.com>
2021-04-07 11:33:02 +0900

    210407 login, logout #1

---
+++ user/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+from MySQLdb import connect, OperationalError
+from MySQLdb.cursors import DictCursor
+
+
+def findbyno():
+    pass
+
+
+def findby_email_and_password(email, password):
+    try:
+        # 연결
+        db = conn()
+
+        # cursor 생성
+        cursor = db.cursor(DictCursor)
+
+        # SQL 실행
+        sql = 'select no, name from user where email=%s and password = %s'
+        cursor.execute(sql, (email, password))
+
+        # 결과 받아오기
+        result = cursor.fetchone()
+
+        # 자원 정리
+        cursor.close()
+        db.close()
+
+        # 결과 반환
+        return result
+
+    except OperationalError as e:
+        print(f'error: {e}')
+
+
+def insert(name, email, password, gender):
+    try:
+        # 연결
+        db = conn()
+
+        # cursor 생성
+        cursor = db.cursor()
+
+        # SQL 실행
+        sql = 'insert into user values(null, %s, %s, %s, %s, now())'
+        count = cursor.execute(sql, (name, email, password, gender))
+
+        # commit
+        db.commit()
+
+        # 자원 정리
+        cursor.close()
+        db.close()
+
+        # 결과 반환
+        return count == 1
+
+    except OperationalError as e:
+        print(f'error: {e}')
+
+
+def update(name, password):
+    pass
+
+
+def conn():
+    return connect(
+        user='webdb',
+        password='webdb',
+        host='localhost',
+        port=3306,
+        db='webdb',
+        charset='utf8')
---
+++ user/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from user import models
+
+
+def joinform(request):
+    return render(request, 'user/joinform.html')
+
+def joinsuccess(request):
+    return render(request, 'user/joinsuccess.html')
+
+def join(request):
+    name = request.POST["name"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+    gender = request.POST["gender"]
+
+    models.insert(name, email, password, gender)
+
+    return HttpResponseRedirect('/user/joinsuccess.html')
+
+def loginform(request):
+    return render(request, 'user/loginform.html')
+
+def login(request):
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    result = models.findby_email_and_password(email,password)
+    if result is None:
+        return HttpResponseRedirect('loginform?result=fail')
+
+
+    # login 처리
+    # csrftoken 으로 쿠키를 남겨놈. 얘는 계속 요청을 함. 그러니까 거기에 정보를 넣어놓으면 계속 돌아가니까 로그인? 대충 이런 느낌
+    request.session["authuser"] = result # 'authuser' 라는 객체로 계속 요청을 해줌. 로그아웃은 이걸 없애는거
+
+    return HttpResponseRedirect('/')
+
+def logout(request):
+    del request.session["authuser"]
+    # 저기에 넣어놨던 정보를 지우니까 계속 요청을 해도 암것도 없으니까 로그아웃됨
+    return HttpResponseRedirect('/')
